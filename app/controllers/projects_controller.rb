965a39bebb8a646f0cc81800f0d4f465ab7a4f7e
Ayaan Shahid <ayaanshahid222@gmail.com>
2024-09-07 17:30:53 +0500

    Project CRUD

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,53 @@
 class ProjectsController < ApplicationController
   def index
+    @projects = Project.all
   end
+
+  def show
+    find_project
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+
+    if @project.save
+      redirect_to @project
+    else
+      render :new, status: :unprocessable_entity  
+    end
+  end
+
+  def edit
+    find_project
+  end
+
+  def update
+    find_project
+
+    if @project.update(project_params)
+      redirect_to @project
+    else
+      render :edit, status: :unprocessable_entity  
+    end
+  end
+
+  def destroy
+    find_project
+    @project.destroy
+    redirect_to root_path, status: :see_other
+  end
+
+  
+  private
+    def project_params
+      params.require(:project).permit(:title, :description, :start_date, :end_date)
+    end
+
+    def find_project
+      @project = Project.find(params[:id])
+    end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,24 @@
 class Project < ApplicationRecord
+
+  def formatted_start_date
+    start_date.strftime("%d-%m-%y") if start_date
+  end
+
+  def formatted_end_date
+    end_date.strftime("%d-%m-%y") if end_date
+  end
+
+  validates :title, presence: true
+  validates :description, presence: true  #, length: { minimum: 40 }
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+
+  validate :end_date_after_start_date    # To ensure end date is after start date
+
+
+  def end_date_after_start_date
+    if end_date.present? && start_date.present? && end_date < start_date
+      errors.add(:end_date, "must be after the start date")
+    end
+  end
 end
