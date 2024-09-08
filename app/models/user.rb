41b63e6fa90ee7be331b72ea8b27cf2ea021e498
Ayaan Shahid <ayaanshahid222@gmail.com>
2024-09-08 16:43:38 +0500

    Completed role addition (also added null constraint)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum role: { manager: 0, qa: 1, developer: 2 }
+  validates :role, presence: true, inclusion: { in: roles.keys }
 end
